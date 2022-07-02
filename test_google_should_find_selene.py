f2c7de7aa901cf40df011faf0c7816f63b02d085
xacker <egor_kinderr@mail.ru>
2022-07-02 15:12:13 +0300

    change def test_google_should_find_selene

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -5,4 +5,4 @@ from selene import be, have
 def test_google_should_find_selene():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
